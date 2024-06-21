b33b8b05750125c05f8a60399228e99f42dba353
JuliaAme <kljukv@gmail.com>
2024-06-21 15:20:58 +0300

    fix photos component

--- client/src/page/photo/Photos.jsx
+++ client/src/page/photo/Photos.jsx
@@ -2,26 +2,28 @@ import React, { useEffect, useState } from'react';
 import requestAxios from '../../services/axios';
 import PhotoItem from './PhotoItem';
 import { useParams } from 'react-router-dom';
+import FormAddPhoto from './FormAddPhoto';
 function Photos({ albums }) {
     const { albumId } = useParams()
     const [photos, setPhotos] = useState([])
     const axiosPhotos = async () => {
         const { data } = await requestAxios.get(`/photos/${albumId}`)
         setPhotos(data.photo);
-        console.log(data);
+        
     } 
     
     useEffect(() => {
         axiosPhotos()
+
     }, []);
+
   return (
       <div>
         <h1>Photos</h1>
-
+          <FormAddPhoto setPhotos={ setPhotos} />
         {photos && photos.map((photo) => (
-            <PhotoItem key={ photo.id } photo = { photo }/>
+            <PhotoItem key={photo.id} photo={photo} setPhotos={setPhotos } />
         ))}
-
         </div>
   );
 }
